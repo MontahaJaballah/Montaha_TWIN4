4d6bf70bd52ab5193a31dcd423c39c0f2c47174d
MontahaJaballah <montahabenjabalah@gmail.com>
2024-09-22 11:58:22 +1030

    test

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Montaha_benJaballah_TWIN4';
+}
